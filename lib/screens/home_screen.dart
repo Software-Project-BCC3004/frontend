5e04f0dbe016cf8561165111cab67574844744a1
cDanx <daniel25agm@hotmail.com>
2025-02-27 23:47:29 -0300

    Integração de paciente #3

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -30,7 +30,10 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _screens[_selectedIndex],
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: _screens,
+      ),
       bottomNavigationBar: NavigationBar(
         selectedIndex: _selectedIndex,
         onDestinationSelected: _onItemTapped,
